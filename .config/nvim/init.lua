af98733268ddce19b34e97bdfe19672996b2efa9
Harsh Sahu <yesh8harsh+github@gmail.com>
2026-01-26 01:27:26 +0530

    feat[NVIM]: added default nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,28 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- Default options:
+require("gruvbox").setup({
+  terminal_colors = true,
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    emphasis = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  inverse = true,
+  contrast = "hard",
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = false,
+})
+vim.cmd("colorscheme gruvbox")
