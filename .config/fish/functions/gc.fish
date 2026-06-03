0892cd210e2253958993f52ff691bfbb5432c9b7
Harsh Sahu <yesh8harsh+github@gmail.com>
2026-06-04 00:12:56 +0530

    fix[GC]: add status after gc

--- .config/fish/functions/gc.fish
+++ .config/fish/functions/gc.fish
@@ -1,7 +1,9 @@
-function gc --description "git commit with automatic quote wrapping"
+function gc --description "git commit with automatic quote wrapping and status check"
     if test (count $argv) -eq 0
         git commit
     else
-        git commit -m "$argv"
+        git commit -m (string join " " $argv)
     end
+    echo # print blank line for spacing
+    git status --short
 end
