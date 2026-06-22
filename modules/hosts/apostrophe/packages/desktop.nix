a8df5f01abf9597e039004624031daf704a92752
Harsh Sahu <yesh8harsh+github@gmail.com>
2026-06-22 17:53:15 +0530

    feat[NIX]: fix kvantum package name
    - libsForQt5.kvantum → libsForQt5.qtstyleplugin-kvantum

--- modules/hosts/apostrophe/packages/desktop.nix
+++ modules/hosts/apostrophe/packages/desktop.nix
@@ -24,7 +24,7 @@
       # -- Qt / GTK Themes --
       qt6Packages.qt6ct
       libsForQt5.qt5ct
-      libsForQt5.kvantum
+      libsForQt5.qtstyleplugin-kvantum
       papirus-icon-theme
       papirus-folders
       capitaine-cursors
