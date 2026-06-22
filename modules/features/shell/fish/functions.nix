42de65066479ff53a8d494716853ca5018bd0153
Harsh Sahu <yesh8harsh+github@gmail.com>
2026-06-22 17:47:59 +0530

    feat[NIX]: fix fish keybindings - move to interactiveShellInit
    - interactiveShellAbbrs doesn't exist in Home Manager
    - Moved !! and !$ keybindings to interactiveShellInit with bind commands

--- modules/features/shell/fish/functions.nix
+++ modules/features/shell/fish/functions.nix
@@ -100,10 +100,15 @@
           '';
         };
       };
-      interactiveShellAbbrs = {
-        "!!" = "__history_previous_command";
-        "!$" = "__history_previous_command_arguments";
-      };
+      interactiveShellInit = ''
+        if [ "$fish_key_bindings" = fish_vi_key_bindings ]
+            bind -Minsert ! __history_previous_command
+            bind -Minsert '$' __history_previous_command_arguments
+        else
+            bind ! __history_previous_command
+            bind '$' __history_previous_command_arguments
+        end
+      '';
     };
   };
 }
