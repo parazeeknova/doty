fa09bacf1705f7fb995d8479402033e4cd8520fa
Harsh Sahu <yesh8harsh+github@gmail.com>
2026-06-22 16:41:38 +0530

    feat[NIX]: add grouping comments to fish env and aliases
    - env.nix: Locale, SSH/GPG, Man Pages, Notifications, Docker, Paths, Wabi, NVM
    - aliases.nix: File Ops, Navigation, VPN, Dotfiles, File Listing, Search,
      Arch/Pacman, System/Services, Editors, TMUX, JS/TS, Rust, Archives, Git

--- modules/features/shell/fish/aliases.nix
+++ modules/features/shell/fish/aliases.nix
@@ -4,30 +4,45 @@
 
     home-manager.users.parazeeknova.programs.fish = {
       shellAliases = {
+        # -- File Operations --
         cp = "cp -iv";
         mkdir = "mkdir -pv";
         mv = "mv -iv";
         rm = "rm -rf";
+
+        # -- Navigation --
         ".." = "cd ..";
         "..." = "cd ../..";
         "...." = "cd ../../..";
         "....." = "cd ../../../..";
         "......" = "cd ../../../../..";
+
+        # -- Clear --
         cl = "clear";
+
+        # -- Warp VPN --
         cfon = "warp-cli connect";
         cfoff = "warp-cli disconnect";
+
+        # -- Dotfiles --
         doty = "cd ~/doty && make sync";
+
+        # -- File Listing (eza) --
         ls = "exa --color=auto --icons";
         la = "exa -la --color=auto --icons";
         ll = "exa -alh --color=auto --icons";
         lt = "exa -a --tree --color=auto --icons";
         dir = "dir --color=auto";
         vdir = "vdir --color=auto";
+
+        # -- Search & Info --
         grep = "grep --color=auto";
         hw = "hwinfo --short";
         psmem = "ps auxf | sort -nr -k 4";
         psmem10 = "ps auxf | sort -nr -k 4 | head -10";
         jctl = "journalctl -p 3 -xb";
+
+        # -- Arch Linux / Pacman / Paru --
         ahh = "paru";
         ahhh = "paru -S";
         nah = "paru -R";
@@ -38,21 +53,27 @@
         big = "expac -H M '%m\\t%n' | sort -h | nl";
         gitpkg = "pacman -Q | grep -i \"\\-git\" | wc -l";
         rip = "expac --timefmt='%Y-%m-%d %T' '%l\\t%n %v' | sort | tail -200 | nl";
-        "+cuts" = "nvim ~/doty/.config/fish/conf.d/aliases.fish";
-        ocfig = "nvim ~/doty/.config/opencode/opencode.json";
         pewup = "sudo pacman -Syyu";
         pewrm = "sudo pacman -R";
         pewin = "sudo pacman -S";
         pewsr = "sudo pacman -Ss";
+
+        # -- System / Services --
         ctl = "sudo systemctl";
         uctl = "systemctl --user";
         pew = "sudo";
+
+        # -- Editors & Dev Tools --
         nv = "nvim";
         ai = "opencode";
         fishy = "nvim ~/doty/.config/fish/config.fish";
         ghosy = "nvim ~/doty/.config/ghostty/config";
         fasty = "fastfetch";
+
+        # -- TMUX --
         tx = "tmux";
+
+        # -- JS/TS (Bun/Pnpm) --
         pn = "pnpm";
         buni = "bun install";
         bunc = "bun check";
@@ -60,15 +81,20 @@
         bunt = "bun run test";
         brr = "bun run dev";
         bct = "bun run check && bun run check-types";
+
+        # -- Rust (Cargo) --
         cgin = "cargo install";
         cginit = "cargo init";
         cgb = "cargo build";
         cgr = "cargo run";
+
+        # -- Archives --
         tarnow = "tar -acf ";
         untar = "tar -zxvf ";
         wget = "wget -c ";
       };
       shellAbbrs = {
+        # -- Git --
         g = "git";
         ga = "git add";
         gc = "git commit";
@@ -77,9 +103,13 @@
         gl = "git log --oneline --graph --decorate -20";
         gp = "git push";
         gs = "git status";
+
+        # -- TMUX --
         t = "tmux";
         ta = "tmux attach -t";
         tn = "tmux new-session -s";
+
+        # -- General --
         v = "nvim";
         c = "clear";
         lg = "lazygit";
--- modules/features/shell/fish/env.nix
+++ modules/features/shell/fish/env.nix
@@ -4,29 +4,39 @@
 
     home-manager.users.parazeeknova.programs.fish = {
       shellInit = ''
+        # -- Locale --
         set -gx LANG en_US.UTF-8
         set -gx LC_ALL en_US.UTF-8
+
+        # -- SSH / GPG --
         set -gx GPG_TTY (tty)
         set -gx SSH_ASKPASS /usr/lib/seahorse/ssh-askpass
         set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
-        set -gx PATH $HOME/.npm-global/bin $PATH
+
+        # -- Man Pages --
         set -x MANROFFOPT -c
         set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+        # -- Notifications --
         set -U __done_min_cmd_duration 10000
         set -U __done_notification_urgency_level low
-        export PATH="$HOME/.bun/bin:$PATH"
+
+        # -- Docker --
         export DOCKER_COMPOSE_PROVIDER_WARNING=0
+
+        # -- Paths --
         fish_add_path ~/.local/bin
         fish_add_path ~/.cargo/bin
-        fish_add_path ~/Applications/depot_tools
-        fish_add_path ~/.spicetify
         fish_add_path ~/.bun/bin
-        fish_add_path ~/.lmstudio/bin
         fish_add_path /home/parazeeknova/.mimocode/bin
+
+        # -- Wabi Theme System --
         set -Ux WABI_DOTFILES_DIR "$HOME/doty"
-        set -Ux WABI_VM_SCAN_ROOT "/run/media/parazeeknova/clips/VM"
+        set -Ux WABI_VM_SCAN_ROOT ""
         set -Ux WABI_GITHUB_USER "parazeeknova"
         set -Ux WABI_PRESETS_DIR "$HOME/doty/.config/hypr/wabi/presets"
+
+        # -- Node Version Manager --
         set --query XDG_DATA_HOME || set --local XDG_DATA_HOME ~/.local/share
         set --query nvm_mirror || set --global nvm_mirror https://nodejs.org/dist
         set --query nvm_data || set --global nvm_data $XDG_DATA_HOME/nvm
