c6d39b9ee6e3b3dff33a2899753de0a9246c5bfe
Harsh Sahu <yesh8harsh+github@gmail.com>
2026-06-23 19:34:27 +0530

    cleanup: remove legacy fish and kilo configuration files

--- .config/fish/conf.d/wabi-env.fish
+++
@@ -1,4 +0,0 @@
-set -Ux WABI_DOTFILES_DIR "$HOME/doty"
-set -Ux WABI_VM_SCAN_ROOT "/run/media/parazeeknova/clips/VM"
-set -Ux WABI_GITHUB_USER "parazeeknova"
-set -Ux WABI_PRESETS_DIR "$HOME/doty/wabi/presets"
--- .config/fish/functions/gc.fish
+++
@@ -1,9 +0,0 @@
-function gc --description "git commit with automatic quote wrapping and status check"
-    if test (count $argv) -eq 0
-        git commit
-    else
-        git commit -m (string join " " $argv)
-    end
-    echo # print blank line for spacing
-    git status --short
-end
