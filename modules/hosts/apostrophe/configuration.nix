7c3867013090f9b35540171e8eb0bc68abb051f8
Harsh Sahu <yesh8harsh+github@gmail.com>
2026-06-25 02:12:41 +0530

    feat[NIX]: add NixOS CUDA binary cache to prevent local CUDA builds

--- modules/hosts/apostrophe/configuration.nix
+++ modules/hosts/apostrophe/configuration.nix
@@ -58,6 +58,14 @@
         "nix-command"
         "flakes"
       ];
+      nix.settings.substituters = [
+        "https://cache.nixos.org"
+        "https://cache.nixos-cuda.org"
+      ];
+      nix.settings.trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+      ];
 
       # -- Boot --
       boot.loader.systemd-boot.enable = false;
