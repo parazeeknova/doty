685bb87c70d2b28e7a7d4a1a267f7cb87932e7e9
Harsh Sahu <yesh8harsh+github@gmail.com>
2026-06-24 14:35:15 +0530

    chore(hyprland): remove broken hyprland-scroll-overview plugin again

--- flake.nix
+++ flake.nix
@@ -12,16 +12,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    hyprland = {
-      url = "github:hyprwm/Hyprland/a0136d8c04687bb36eb8a28eb9d1ff92aea99704";
-    };
-
-    # TODO: Change this back to "github:yayuuu/hyprland-scroll-overview" once PR #27 is merged.
-    hyprland-scroll-overview = {
-      url = "git+https://github.com/yayuuu/hyprland-scroll-overview?ref=refs/pull/27/head";
-      inputs.hyprland.follows = "hyprland";
-    };
-
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs = {
