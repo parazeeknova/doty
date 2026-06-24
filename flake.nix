6a13b690dd96e013f312ec90f26d658fd10487c9
Harsh Sahu <yesh8harsh+github@gmail.com>
2026-06-24 23:55:45 +0530

    feat[DEV]: added devshells for required output packages

--- flake.nix
+++ flake.nix
@@ -28,5 +28,43 @@
     };
   };
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    imports = [ (inputs.import-tree ./modules) ];
+    systems = [ "x86_64-linux" ];
+    perSystem = { config, pkgs, ... }: {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          pkg-config
+          dbus
+          openssl
+          glib
+          gtk3
+          webkitgtk_4_1
+          librsvg
+          wayland
+          libdecor
+          libglvnd
+          mesa
+          xdotool
+          alsa-lib
+        ];
+
+        shellHook = ''
+          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath (with pkgs; [
+            dbus
+            openssl
+            glib
+            gtk3
+            webkitgtk_4_1
+            librsvg
+            wayland
+            libdecor
+            libglvnd
+            mesa
+            alsa-lib
+          ])}:$LD_LIBRARY_PATH
+        '';
+      };
+    };
+  };
 }
