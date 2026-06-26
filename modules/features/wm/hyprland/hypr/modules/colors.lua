81319adc0a38d4afe66dd5872e0dc5ebee880fb8
Harsh Sahu <yesh8harsh+github@gmail.com>
2026-06-26 19:00:40 +0530

    feat[SYSTEMD]: themed limine bootlodeer

--- modules/features/wm/hyprland/hypr/modules/colors.lua
+++ modules/features/wm/hyprland/hypr/modules/colors.lua
@@ -1,8 +1,8 @@
 return {
-	active_border = "rgba(bcc3ffee)",
-	inactive_border = "rgba(46464faa)",
-	border_color = "rgb(bcc3ff)",
-	shadow_color = "0x18bcc3ff",
-	accent_hex = "bcc3ff",
-	bg_light_hex = "46464f",
+    active_border = "rgba(bcc3ffee)",
+    inactive_border = "rgba(46464faa)",
+    border_color = "rgb(bcc3ff)",
+    shadow_color = "0x18bcc3ff",
+    accent_hex = "bcc3ff",
+    bg_light_hex = "46464f"
 }
