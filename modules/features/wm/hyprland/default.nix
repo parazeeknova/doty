6c39cabf8c39ed3fddf33d848fbb5e56322a07d0
Harsh Sahu <yesh8harsh+github@gmail.com>
2026-06-28 17:06:59 +0530

    refactor: split custom plugin derivations into plugins.nix

--- modules/features/wm/hyprland/default.nix
+++ modules/features/wm/hyprland/default.nix
@@ -8,87 +8,7 @@
       ...
     }:
     let
-      hypr-kinetic-scroll = pkgs.stdenv.mkDerivation {
-        pname = "hypr-kinetic-scroll";
-        version = "unstable";
-
-        src = inputs.hypr-kinetic-scroll;
-
-        nativeBuildInputs = [ pkgs.pkg-config ];
-        buildInputs = [
-          pkgs.hyprland
-          pkgs.aquamarine
-          pkgs.hyprgraphics
-          pkgs.hyprutils
-          pkgs.hyprlang
-          pkgs.hyprcursor
-          pkgs.libGL
-          pkgs.libxcb-wm
-          pkgs.libxcb-errors
-          pkgs.wayland-protocols
-          pkgs.lua
-          pkgs.pixman
-          pkgs.libdrm
-          pkgs.libinput
-          pkgs.systemd
-          pkgs.wayland
-          pkgs.libxkbcommon
-          pkgs.pango
-          pkgs.cairo
-        ];
-
-        buildPhase = ''
-          runHook preBuild
-          make
-          runHook postBuild
-        '';
-
-        installPhase = ''
-          runHook preInstall
-          mkdir -p $out/lib
-          cp hypr-kinetic-scroll.so $out/lib/libhypr-kinetic-scroll.so
-          runHook postInstall
-        '';
-      };
-
-      hyprland-scroll-overview = pkgs.stdenv.mkDerivation {
-        pname = "hyprland-scroll-overview";
-        version = "unstable";
-
-        src = inputs.hyprland-scroll-overview;
-
-        nativeBuildInputs = [
-          pkgs.pkg-config
-          pkgs.cmake
-          pkgs.gcc14
-        ];
-        buildInputs = [
-          pkgs.hyprland
-          pkgs.aquamarine
-          pkgs.hyprgraphics
-          pkgs.hyprutils
-          pkgs.hyprlang
-          pkgs.hyprcursor
-          pkgs.libGL
-          pkgs.libxcb-wm
-          pkgs.libxcb-errors
-          pkgs.wayland-protocols
-          pkgs.lua5_4
-          pkgs.pixman
-          pkgs.libdrm
-          pkgs.libinput
-          pkgs.systemd
-          pkgs.wayland
-          pkgs.libxkbcommon
-          pkgs.pango
-          pkgs.cairo
-          pkgs.glslang
-        ];
-
-        postInstall = ''
-          mv $out/lib/libscrolloverview.so $out/lib/libhyprland-scroll-overview.so
-        '';
-      };
+      customPlugins = import ./plugins.nix { inherit pkgs inputs; };
     in
     {
 
@@ -132,8 +52,8 @@
           plugins = [
             pkgs.hyprlandPlugins.hypr-dynamic-cursors
             pkgs.hyprlandPlugins.hyprfocus
-            hypr-kinetic-scroll
-            hyprland-scroll-overview
+            customPlugins.hypr-kinetic-scroll
+            customPlugins.hyprland-scroll-overview
           ];
           extraConfig = ''
             -- Load main hyprland configuration modules
---
+++ modules/features/wm/hyprland/plugins.nix
@@ -0,0 +1,83 @@
+{ pkgs, inputs }: {
+  hypr-kinetic-scroll = pkgs.stdenv.mkDerivation {
+    pname = "hypr-kinetic-scroll";
+    version = "unstable";
+
+    src = inputs.hypr-kinetic-scroll;
+
+    nativeBuildInputs = [ pkgs.pkg-config ];
+    buildInputs = [
+      pkgs.hyprland
+      pkgs.aquamarine
+      pkgs.hyprgraphics
+      pkgs.hyprutils
+      pkgs.hyprlang
+      pkgs.hyprcursor
+      pkgs.libGL
+      pkgs.libxcb-wm
+      pkgs.libxcb-errors
+      pkgs.wayland-protocols
+      pkgs.lua
+      pkgs.pixman
+      pkgs.libdrm
+      pkgs.libinput
+      pkgs.systemd
+      pkgs.wayland
+      pkgs.libxkbcommon
+      pkgs.pango
+      pkgs.cairo
+    ];
+
+    buildPhase = ''
+      runHook preBuild
+      make
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out/lib
+      cp hypr-kinetic-scroll.so $out/lib/libhypr-kinetic-scroll.so
+      runHook postInstall
+    '';
+  };
+
+  hyprland-scroll-overview = pkgs.stdenv.mkDerivation {
+    pname = "hyprland-scroll-overview";
+    version = "unstable";
+
+    src = inputs.hyprland-scroll-overview;
+
+    nativeBuildInputs = [
+      pkgs.pkg-config
+      pkgs.cmake
+      pkgs.gcc14
+    ];
+    buildInputs = [
+      pkgs.hyprland
+      pkgs.aquamarine
+      pkgs.hyprgraphics
+      pkgs.hyprutils
+      pkgs.hyprlang
+      pkgs.hyprcursor
+      pkgs.libGL
+      pkgs.libxcb-wm
+      pkgs.libxcb-errors
+      pkgs.wayland-protocols
+      pkgs.lua5_4
+      pkgs.pixman
+      pkgs.libdrm
+      pkgs.libinput
+      pkgs.systemd
+      pkgs.wayland
+      pkgs.libxkbcommon
+      pkgs.pango
+      pkgs.cairo
+      pkgs.glslang
+    ];
+
+    postInstall = ''
+      mv $out/lib/libscrolloverview.so $out/lib/libhyprland-scroll-overview.so
+    '';
+  };
+}
