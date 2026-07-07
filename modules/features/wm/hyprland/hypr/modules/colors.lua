c518e89567888f150703e3efdbc324de0843a999
Harsh Sahu <yesh8harsh+github@gmail.com>
2026-07-07 10:05:16 +0530

    chore[WABI]: switched default theme

--- modules/features/wm/hyprland/hypr/modules/colors.lua
+++ modules/features/wm/hyprland/hypr/modules/colors.lua
@@ -1,8 +1,8 @@
 return {
-	active_border = "rgba(bcc3ffee)",
-	inactive_border = "rgba(46464faa)",
-	border_color = "rgb(bcc3ff)",
-	shadow_color = "0x18bcc3ff",
-	accent_hex = "bcc3ff",
-	bg_light_hex = "46464f",
+	active_border = "rgba(f8bb71ee)",
+	inactive_border = "rgba(504539aa)",
+	border_color = "rgb(f8bb71)",
+	shadow_color = "0x18f8bb71",
+	accent_hex = "f8bb71",
+	bg_light_hex = "504539",
 }
