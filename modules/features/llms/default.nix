980d4294110241ff076495098ec8bd7dc0f3be3e
Harsh Sahu <yesh8harsh+github@gmail.com>
2026-07-26 17:13:22 +0530

    chore[CUDA]: adding cuda support for llamacpp

--- modules/features/llms/default.nix
+++ modules/features/llms/default.nix
@@ -1,37 +1,55 @@
-{ self, inputs, ... }: {
+{ self, inputs, ... }:
 
+{
   flake.nixosModules.parazeeknovaLlms =
-    {
-      config,
-      pkgs,
-      lib,
-      ...
-    }:
+    { pkgs, ... }:
+
     {
       environment.systemPackages = with pkgs; [
         pi-coding-agent
         codex
         github-copilot-cli
-        llama-cpp
         lmstudio
         yt-dlp
         cudatoolkit
+
+        (llama-cpp.override {
+          cudaSupport = true;
+        })
+        ollama-cuda
       ];
 
       services.ollama = {
         enable = true;
+        package = pkgs.ollama-cuda;
       };
 
       services.open-webui = {
         enable = true;
         package = pkgs.open-webui;
-        stateDir = "/var/lib/open-webui";
-        port = 8181;
+
         host = "127.0.0.1";
-        openFirewall = false;
+        port = 8181;
+
         environment = {
           OLLAMA_BASE_URL = "http://127.0.0.1:11434";
         };
+      };
+
+      services.llama-cpp = {
+        enable = true;
+        package = pkgs.llama-cpp.override {
+          cudaSupport = true;
+        };
+        host = "127.0.0.1";
+        port = 8787;
+        extraFlags = [
+          "--flash-attn"
+          "-ngl" "999"
+          "-c" "4096"
+          "-b" "1024"
+          "-t" "8"
+        ];
       };
     };
 }
