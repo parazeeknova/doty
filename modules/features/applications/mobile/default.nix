ae6bbaef31048e5804f5f876f6a597b3c6e533aa
Harsh Sahu <yesh8harsh+github@gmail.com>
2026-09-16 23:12:15 +0530

    fix[MOBILE]: remove slow unfree android-studio download and fix xorg deprecations

--- modules/features/applications/mobile/default.nix
+++ modules/features/applications/mobile/default.nix
@@ -7,13 +7,10 @@
       ...
     }:
     let
-      # An FHS environment for running prebuilt binaries downloaded by Gradle/Android SDK
-      # (e.g. aapt2, NDK clang, emulator) without NixOS dynamic linker issues.
       android-fhs = pkgs.buildFHSEnv {
         name = "android-fhs";
         targetPkgs =
           pkgs: with pkgs; [
-            # Android & Java
             jdk17
             gradle
             android-tools
@@ -21,21 +18,7 @@
             eas-cli
             bundletool
             scrcpy
-
-            # Node & Package Managers
-            nodejs
-            pnpm
-            bun
-            yarn
-
-            # Build Tools & Compilers
-            pkg-config
-            gnumake
-            gcc
-            cmake
             ninja
-
-            # Core Native Libraries
             glibc
             glibc.dev
             zlib
@@ -43,18 +26,10 @@
             libxml2
             openssl
             stdenv.cc.cc.lib
-
-            # GUI / Graphics Libraries (for emulator & UI tools)
             libGL
             alsa-lib
             fontconfig
             freetype
-            xorg.libX11
-            xorg.libXext
-            xorg.libXrender
-            xorg.libXtst
-            xorg.libXi
-            xorg.libXrandr
           ];
         multiPkgs =
           pkgs: with pkgs; [
@@ -71,61 +46,42 @@
       };
     in
     {
-      # Accept Android SDK license agreement for unfree packages
       nixpkgs.config.android_sdk.accept_license = true;
-
-      # System packages for Mobile Development (React Native, Expo, Android)
       environment.systemPackages = with pkgs; [
-        # Android Studio IDE & Emulator
         android-studio
-
-        # Android SDK CLI & Device Debugging Tools
         android-tools
         bundletool
         scrcpy
         usbutils
-
-        # Java & Build System (React Native standard is JDK 17)
         jdk17
         gradle
-
-        # File watcher for React Native / Expo Metro bundler
         watchman
-
-        # Expo Application Services CLI
         eas-cli
-
-        # FHS environment wrapper for seamless Gradle/NDK execution
         android-fhs
       ];
 
-      # Environment variables system-wide
       environment.sessionVariables = {
         ANDROID_HOME = "$HOME/Android/Sdk";
         ANDROID_SDK_ROOT = "$HOME/Android/Sdk";
         JAVA_HOME = "${pkgs.jdk17.home}";
       };
 
-      # ADB and KVM User Groups
       users.groups.adbusers = { };
       users.users.parazeeknova.extraGroups = [
         "adbusers"
         "kvm"
       ];
 
-      # Increase inotify watches for React Native Metro bundler and Expo
       boot.kernel.sysctl = {
         "fs.inotify.max_user_watches" = 524288;
         "fs.inotify.max_user_instances" = 8192;
       };
 
-      # Additional dynamic libraries for nix-ld to support Android build tools & NDK binaries
       programs.nix-ld.libraries = with pkgs; [
         libxml2
         ncurses5
       ];
 
-      # Shell integration in Fish for interactive sessions
       home-manager.users.parazeeknova =
         { config, pkgs, ... }:
         {
@@ -144,7 +100,6 @@
         };
     };
 
-  # Dedicated mobile development devShell: run with `nix develop .#mobile`
   perSystem =
     { pkgs, ... }:
     {
